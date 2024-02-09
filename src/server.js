d0a22a21f443fd60fd63dc4ed60aec7ea16e03d7
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-02-09 12:15:48 -0400

    add cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 require('./services/database');
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(cors());
 // eslint-disable-next-line no-undef
 const port = process.env.SERVER_PORT;
 
